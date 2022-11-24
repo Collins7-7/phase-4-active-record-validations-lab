7050dad77c16bd174b06a9ed924cddca06e5425e
COllins <collinsmuiruri77@gmail.com>
2022-11-24 13:42:20 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length:{minimum: 250}
+    validates :summary, length:{maximum: 250}
+    validates :category, inclusion:{in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_not
+
+
+    def title_is_not
+        if title == "True Facts"
+            errors.add(:title, "Only clickbaits allowed")
+        end
+    end
 end
